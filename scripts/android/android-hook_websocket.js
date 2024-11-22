d1c5e35658b62b51096ae0a088aa2dc9e8a79704
Andreas Knudsen Alstrp <aalstr20@student.aau.dk>
2024-11-22 14:50:20 +0100

    Added SignalServiceMessageSender

--- scripts/android/android-hook_websocket.js
+++ scripts/android/android-hook_websocket.js
@@ -1,4 +1,3 @@
-
 function appendToFile(filePath, string) {
         var String = Java.use('java.lang.String');
         var FileOutputStream = Java.use('java.io.FileOutputStream');
@@ -30,46 +29,19 @@ Java.perform(function() {
         let logFile = "/storage/emulated/0/signal-websocket.log";
 
         console.log("[*] Hooking OkHttpWebSocketConnection...");
-
         const OkHttpWebSocketConnection = Java.use("org.whispersystems.signalservice.internal.websocket.OkHttpWebSocketConnection");
 
         OkHttpWebSocketConnection.sendRequest.implementation = function(request) {
                 console.log("[*] sendRequest called!");
 
-                // var WebSocketMessage = Java.use("org.whispersystems.signalservice.internal.websocket.WebSocketMessage");
-                // var Builder = Java.use("org.whispersystems.signalservice.internal.websocket.WebSocketMessage$Builder");
-                //
-                // // Load the WebSocketMessage$Type class and select the "REQUEST" type
-                // var WebSocketMessageType = Java.use("org.whispersystems.signalservice.internal.websocket.WebSocketMessage$Type");
-                // var requestType = WebSocketMessageType.REQUEST.value;
-                //
-                // // Create an instance of WebSocketRequestMessage (or replace with an existing instance)
-                // var WebSocketRequestMessage = Java.use("org.whispersystems.signalservice.internal.websocket.WebSocketRequestMessage");
-                // var requestInstance = WebSocketRequestMessage.$new();
-                //
-                // // Create the Builder instance and set its properties
-                // var builder = Builder.$new(); // Create the builder
-                // builder.type(requestType);    // Set type to REQUEST
-                // builder.request(requestInstance); // Set the request instance
-                //
-                // // Build the WebSocketMessage
-                // var webSocketMessage = builder.build();
-
-                // Log the constructed WebSocketMessage
-                // console.log("[****] WebSocketMessage constructed:", webSocketMessage);
-
                 try {
                         // Log
-                        let logging = request.toString();
-                        console.log("[*] WebSocketRequestMessage:", logging);
-                        appendToFile(logFile, logging);
-
-                        // Call original method
-                        const res = this.sendRequest(request);
-                        console.log("[*] Method response:", res);
+                        appendToFile(logFile, '"Called sendRequest [request]": {\n' + request.toString() + '"\n}');
+                        console.log("[*] sendRequest:", request.toString());
 
                         // Give control back
-                        return res;
+                        return this.sendRequest(request);
+
                 } catch (e) {
                         console.error("[!] Exception in sendRequest:", e);
                         throw e;
@@ -81,18 +53,13 @@ Java.perform(function() {
 
                 try {
                         // Log
-                        let logging = response.toString();
-                        console.log("[*] WebSocketResponseMessage:", logging);
-                        appendToFile(logFile, logging);
-
-                        // Call original method
-                        const res = this.sendResponse(response);
-                        console.log("[*] Method response:", res);
+                        appendToFile(logFile, '"Called sendResponse [response]": {\n' + response.toString() + '"\n}');
+                        console.log("[*] sendResponse:", response.toString());
 
                         // Give control back
-                        return res;
+                        return this.sendResponse(response);
                 } catch (e) {
-                        console.error("[!] Exception in sendRequest:", e);
+                        console.error("[!] Exception in sendResponse:", e);
                         throw e;
                 }
         };
@@ -100,33 +67,46 @@ Java.perform(function() {
         OkHttpWebSocketConnection.onMessage.overload('okhttp3.WebSocket', 'okio.ByteString').implementation = function(webSocket, payload) {
                 console.log("[*] onMessage called!");
 
-                var WebSocketMessage = Java.use("org.whispersystems.signalservice.internal.websocket.WebSocketMessage");
-                var adapter = WebSocketMessage.ADAPTER.value;
-                // var ByteString = Java.use("okio.ByteString");
-                var byteArray = payload.toByteArray();
-                var message = adapter.decode(byteArray);
-
-                console.log("[!!!!] Decoded WebSocketMessage:", message);
-
                 try {
-                        // // Log 
-                        // let logging = webSocket.queueSize().toString();
-                        // appendToFile(logFile, "queueSize: " + logging);
-                        //
-                        // logging = webSocket.request().toString();
-                        // appendToFile(logFile, "request: \n" + logging);
-                        //
-                        // logging = payload;
-                        // appendToFile(logFile, "payload: \n" + logging);
+                        // Create webSocketMessage
+                        var WebSocketMessage = Java.use("org.whispersystems.signalservice.internal.websocket.WebSocketMessage");
+                        var adapter = WebSocketMessage.ADAPTER.value;
+                        var byteArray = payload.toByteArray();
+                        var message = adapter.decode(byteArray);
 
-                        // Call original method
-                        const res = this.onMessage(webSocket, payload);
-                        console.log("[*] Method response:", res);
+                        // Log 
+                        appendToFile(logFile, '"Called onMessage [webSocketMessage]": {\n' + message + '"\n}');
+                        console.log("[*] webSocketMessage:", message);
+
+                        appendToFile(logFile, '"Called onMessage [payload]": {\n' + payload + '"\n}');
+                        console.log("[*] payload:", payload);
 
                         // Give control back
-                        return res;
+                        return this.onMessage(webSocket, payload);
                 } catch (e) {
-                        console.error("[!] Exception in sendRequest:", e);
+                        console.error("[!] Exception in onMessage:", e);
+                        throw e;
+                }
+        };
+
+        console.log("[*] Hooking SingalServiceMessageSender...");
+        const SignalServiceMessageSender = Java.use("org.whispersystems.signalservice.api.SignalServiceMessageSender");
+
+        SignalServiceMessageSender.sendContent.implementation = function(recipient, sealedSenderAccess, contentHint, message, sendEvents, urgent, includePniSignature, content) {
+                console.log("[*] sendContent called!");
+
+                printLoadedClasses();
+
+                try {
+                        console.log("[*] content:", content);
+                        console.log("[*] content:", content.toString());
+                        console.log("[*] message:", message);
+                        console.log("[*] message:", message.toString());
+
+                        // Give control back
+                        return this.sendContent(recipient, sealedSenderAccess, contentHint, message, sendEvents, urgent, includePniSignature, content);
+                } catch (e) {
+                        console.error("[!] Exception in sendContent:", e);
                         throw e;
                 }
         };
